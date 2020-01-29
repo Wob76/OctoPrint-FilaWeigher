2f2a9ba370f56fa5b1d920dca4ee0567e6374dfd
simpat1zq <simit@simitpatel.net>
2020-01-29 13:06:07 -0500

    Update __init__.py

--- octoprint_filaweigher/__init__.py
+++ octoprint_filaweigher/__init__.py
@@ -21,21 +21,13 @@ class filaweigherPlugin(octoprint.plugin.SettingsPlugin,
 	
 	def get_settings_defaults(self):
 		return dict(
-			tare = 8430152,
-			reference_unit=-411,
-			spool_weight=200,
-			clockpin=21,
-			datapin=20,
-			lastknownweight=0
-			
-			# put your plugin's default settings here
+			weight-topic = "your/weight/topic",
+			temperature-topic = "your/temperature/topic",
+			humidity-topic = "your/humidity/topic",
+			pressure-topic = "your/pressure/topic" 
 		)
 
-	##~~ AssetPlugin mixin
-
 	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
 		return dict(
 			js=["js/filaweigher.js"],
 			css=["css/filaweigher.css"],
@@ -54,18 +46,16 @@ class filaweigherPlugin(octoprint.plugin.SettingsPlugin,
 		self.hx.power_down()
 		
 	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
+
 		return dict(
 			filament_scale=dict(
-				displayName="Filament Scale Plugin",
+				displayName="OctoPrint FilaWeigher",
 				displayVersion=self._plugin_version,
 
 				# version check: github repository
 				type="github_release",
-				user="dieki-n",
-				repo="OctoPrint-Filament-scale",
+				user="simpat1zq",
+				repo="OctoPrint-FilaWeigher",
 				current=self._plugin_version,
 
 				# update method: pip
@@ -77,11 +67,11 @@ class filaweigherPlugin(octoprint.plugin.SettingsPlugin,
 # If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
 # ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
 # can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Filament Scale"
+__plugin_name__ = "OctoPrint FilaWeigher"
 
 def __plugin_load__():
 	global __plugin_implementation__
-	__plugin_implementation__ = Filament_scalePlugin()
+	__plugin_implementation__ = filaweigherPlugin()
 
 	global __plugin_hooks__
 	__plugin_hooks__ = {
