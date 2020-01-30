cff9d41819cc9f68251f2e16bdd0d38210c4c796
simpat1zq <simit@simitpatel.net>
2020-01-30 13:49:19 -0500

    Update filaweigher.js

--- octoprint_filaweigher/static/js/filaweigher.js
+++ octoprint_filaweigher/static/js/filaweigher.js
@@ -3,17 +3,20 @@ $(function() {
         var self = this;
 		self.printerState = parameters[0]
 		self.settings = parameters[1]
- 
+
+		//sets the 4 sensors to 'loading'. It will show this until we get a reading from 
+		// the filaweigher 
 		self.printerState.filamentRemainingString = ko.observable("Loading...")
 		self.printerState.temperatureString = ko.observable("Loading...")
 		self.printerState.humidityString = ko.observable("Loading...")
 		self.printerState.pressureString = ko.observable("Loading...")
 
+		//when __init.py sends a plugin message, this function kicks into action
 		self.onDataUpdaterPluginMessage = function(plugin, message){
-			if (plugin != "filaweigher") return;
-			var obj = JSON.parse(message); 
-
-			self.printerState.filamentRemainingString(obj.weight + "g");	
+			if (plugin != "filaweigher") return; //stop if the message wasn't from the filaweigher plugin
+			var obj = JSON.parse(message); //parse the json and load it into obj
+			//set all the sensor values 
+			self.printerState.filamentRemainingString(obj.weight + "g"); 
 			self.printerState.temperatureString(obj.temperature);
 			self.printerState.humidityString(obj.humidity);
 			self.printerState.pressureString(obj.pressure); 
@@ -22,8 +25,9 @@ $(function() {
 		};
 
 		self.onStartup = function() {
+			//This displays all the sensor readings in the gui right after the printer state
+			// on the left side
 			var element = $("#state").find(".accordion-inner [data-bind='text: stateString']");
-
             if (element.length) {
                 //var text = gettext("Filament Remaining");
 				element.after("<br>Filament Remaining: <strong data-bind='text: filamentRemainingString'></strong>");
