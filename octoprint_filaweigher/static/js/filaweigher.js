1a2d1f868e31c730fca9bb1a8beecd5f378a7553
Wob76 <quotidian@gmail.com>
2020-09-27 09:45:00 +1000

    Update filaweigher.js

--- octoprint_filaweigher/static/js/filaweigher.js
+++ octoprint_filaweigher/static/js/filaweigher.js
@@ -9,17 +9,17 @@ $(function() {
 		self.printerState.filamentRemainingString = ko.observable("Loading...")
 		self.printerState.temperatureString = ko.observable("Loading...")
 		self.printerState.humidityString = ko.observable("Loading...")
-		self.printerState.pressureString = ko.observable("Loading...")
+		//self.printerState.pressureString = ko.observable("Loading...")
 
 		//when __init.py sends a plugin message, this function kicks into action
 		self.onDataUpdaterPluginMessage = function(plugin, message){
 			if (plugin != "filaweigher") return; //stop if the message wasn't from the filaweigher plugin
 			var obj = JSON.parse(message); //parse the json and load it into obj
 			//set all the sensor values 
-			self.printerState.filamentRemainingString(obj.weight + "g"); 
-			self.printerState.temperatureString(obj.temperature);
-			self.printerState.humidityString(obj.humidity);
-			self.printerState.pressureString(obj.pressure); 
+			self.printerState.filamentRemainingString(obj.weight.toFixed() + "g"); 
+			self.printerState.temperatureString(obj.temperature.toFixed(1) + "%");
+			self.printerState.humidityString(obj.humidity.toFixed(1) + "C");
+			//self.printerState.pressureString(obj.pressure); 
 	
 			console.log(message);
 		};
@@ -30,10 +30,10 @@ $(function() {
 			var element = $("#state").find(".accordion-inner [data-bind='text: stateString']");
             if (element.length) {
                 //var text = gettext("Filament Remaining");
-				element.after("<br>Filament Remaining: <strong data-bind='text: filamentRemainingString'></strong>");
-				element.after("<br>Room Temperature: <strong data-bind='text: temperatureString'></strong>");
-				element.after("<br>Humidity: <strong data-bind='text: humidityString'></strong>");
-				element.after("<br>Pressure: <strong data-bind='text: pressureString'></strong>");
+				element.after("<br>Printer Humidity: <strong data-bind='text: humidityString'></strong>");
+				element.after("<br>Printer Temperature: <strong data-bind='text: temperatureString'></strong>");
+		    		element.after("<br>Filament Remaining: <strong data-bind='text: filamentRemainingString'></strong>");
+				//element.after("<br>Pressure: <strong data-bind='text: pressureString'></strong>");
             }
         };
 	}
